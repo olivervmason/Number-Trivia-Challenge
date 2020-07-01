dee1046fb5eb297ee251104e413f4a96bec2a263
Oliver Mason <omason@protonmail.com>
2020-07-01 15:58:09 +1000

    Introduce hooks to App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 import Number from './Number';
 
-class App extends React.Component {
-  state = { number: '' };
+// 0. Import useState
 
-  onSearchSubmit = async (numberSel, option) => {
+// 1. Convert from class to function
+const App = () => {
+// Delete line:
+  // state = { number: '' };
+
+// 3. 
+  const [number, setNumber] = useState('')
+
+// 6. Change to const:
+  const onSearchSubmit = async (numberSel, option) => {
     const response = await axios.get(
       `http://numbersapi.com/${numberSel}/${option}`
     );
 
-    this.setState({ number: response.data });
+// 4. Change this line:
+    // this.setState({ number: response.data });
+    setNumber(response.data)
   };
 
-  render() {
+// 5. Get rid of render
+  // render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <Number number={this.state.number} />
+        {/* Change this.onSearchSubmit */}
+        <SearchBar onSubmit={onSearchSubmit} />
+        {/* Change this.state.number */}
+        <Number number={number} />
       </div>
     );
-  }
+  // }
 }
 
 export default App;
